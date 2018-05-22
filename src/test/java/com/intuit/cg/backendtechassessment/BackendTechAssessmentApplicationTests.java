3998f412d8a404bda8d243609a5961a71b49cf8b
AMoghe <advaitmoghe2006@gmail.com>
2018-05-21 20:37:14 -0500

    Add a few tests

--- src/test/java/com/intuit/cg/backendtechassessment/BackendTechAssessmentApplicationTests.java
+++ src/test/java/com/intuit/cg/backendtechassessment/BackendTechAssessmentApplicationTests.java
@@ -1,9 +1,15 @@
 package com.intuit.cg.backendtechassessment;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.intuit.cg.backendtechassessment.controller.BackendTechAssessmentController;
+import com.intuit.cg.backendtechassessment.exception.BadRequestException;
+import com.intuit.cg.backendtechassessment.request.AddProjectRequest;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +19,32 @@ public class BackendTechAssessmentApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testAddProject_NoBidClosingDate() {
+		try {
+			new BackendTechAssessmentController().addProject(new AddProjectRequest(1L, "someDescription", 123.0, null));
+			fail("An exception should be thrown");
+		} catch (BadRequestException exception) {
+
+		}
+	}
+//
+//	@Test
+//	public void testAddProject_NoSellerId() {
+//		try {
+//			Call the service with a AddProjectRequest which does not have a valid seller id 
+//		} catch (BadRequestException exception) {
+//			the service should throw a bad request exception
+//		}
+//	}
+	
+//
+//	@Test
+//	public void testAddBid_NoBuyerId() {
+//		try {
+//			Call the service with a AddBidRequest which does not have a valid buyer id 
+//		} catch (BadRequestException exception) {
+//			the service should throw a bad request exception since the buyer id does not exist 
+//		}
+//	}
 }
